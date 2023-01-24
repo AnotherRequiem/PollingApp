482eba42f9de0232c417763a0cbf3290e19e5444
Requiem <1darkness1337@gmail.com>
2023-01-24 19:05:51 +0200

    created base business logic

---
+++ src/PollingApp/PollingApp.ConsoleClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using PollingApp.ConsoleClient;
+
+var builder = new PollBuilder("How are u?")
+    .AddAnswer(1, "Fine")
+    .AddAnswer(2, "Not bad")
+    .AddAnswer(3, "Awesome")
+    .AddAnswer(4, "Not good at all");
+
+var poll = builder.Build();
+
+poll.VoteTo(3);
+
+poll.VoteTo(3);
+poll.VoteTo(2);
+poll.VoteTo(1);
+poll.VoteTo(4);
+poll.VoteTo(4);
+poll.VoteTo(4);
+poll.VoteTo(4);
+poll.VoteTo(4);
+poll.VoteTo(4, 10);
+
+
+var result = builder.GetResults(poll);
+
+Console.WriteLine(result.GetView());
+
+
+//var poll = new Poll("Как вам это видео?")
+//{
+//    Answers = new List<PollAnswer>
+//    {
+//        new(1,"Нормально"),
+//        new(2,"Не плохо"),
+//        new(3,"Отстой"),
+//        new(4,"Супер")
+//    }
+//};
+
+//poll.VoteTo(3);
+
+//poll.VoteTo(3);
+//poll.VoteTo(2);
+//poll.VoteTo(1);
+//poll.VoteTo(4);
+//poll.VoteTo(4);
+//poll.VoteTo(4);
+//poll.VoteTo(4);
+//poll.VoteTo(4);
+//poll.VoteTo(4, 10);
+
+
+//Console.WriteLine(poll);
---
+++ src/PollingApp/PollingApp/Answer.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+
+namespace PollingApp.Entities;
+
+public class PollAnswer
+{
+    public PollAnswer(int id, string title)
+    {
+        Id = id;
+        Title = title;
+    }
+
+    public int Id { get; }
+
+    public string Title { get; }
+
+    public int Votes { get; set; }
+
+    public double Percents { get; set; }
+
+    public void SetPercents(int totalVotes)
+    {
+        if (totalVotes > 0)
+        {
+            Percents = Votes * 100d / totalVotes;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"* {Title} ({Votes} {Percents:F})";
+    }
+}
